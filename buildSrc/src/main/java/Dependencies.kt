c09e1ae642f4cd7e5d6b5ae83c52ec2bb1bbadf4
kyvk <73049975+kyvk@users.noreply.github.com>
2024-03-06 13:23:56 -0500

    buildSrc

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,26 @@
+package dependencies
+
+object Dependencies {
+    const val namespace = "com.summit.summitproject"
+    const val compileSdk = 34
+    const val applicationId = "com.summit.summitproject"
+    const val minSdk = 29
+    const val targetSdk = 34
+    const val versionCode = 1
+    const val versionName = "1.1"
+    const val jvmTarget = "17"
+    const val gradle = "8.1.1"
+    const val kotlin = "1.5.1"
+    const val compMaterial = "1.5.3"
+    const val kotlinCompilerExtension = "1.5.0"
+    const val core_ktx = "1.12.0"
+    const val appcompat = "1.6.1"
+    const val lifecycle_runtime_ktx = "2.6.2"
+    const val activity_compose = "1.8.0"
+    const val material = "1.10.0"
+    const val compose = "1.5.3"
+    const val retrofit = "2.9.0"
+    const val logging_interceptor = "4.11.0"
+    const val fragment_ktx = "1.6.1"
+
+}
